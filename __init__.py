550443bc74a12e038590331eb339c6428a75c1bf
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2025-11-07 14:48:29 +0100

    Rename product_location_link to product_warehouse_position.
    Task #175066

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-# This file is part product_location_link module for Tryton.
+# This file is part product_warehouse_position module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
@@ -8,4 +8,4 @@ def register():
     Pool.register(
         product.ProductLocationLink,
         product.Template,
-        module='product_location_link', type_='model')
+        module='product_warehouse_position', type_='model')
