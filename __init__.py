fdd4a9dbb0bdae83ddce6d9918bdfe339da164c9
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2026-06-02 14:10:21 +0200

    Make compatible the product_warehouse_location module with the stock_location_company company related fields.
    Task #188330

--- __init__.py
+++ __init__.py
@@ -3,9 +3,15 @@
 # the full copyright notices and license terms.
 from trytond.pool import Pool
 from . import product
+from . import product_location_company
 
 def register():
     Pool.register(
         product.ProductPosition,
         product.Template,
         module='product_warehouse_position', type_='model')
+    Pool.register(
+        product_location_company.ProductPosition,
+        product_location_company.Template,
+        module='product_warehouse_position', type_='model',
+        depends=['stock_location_company'])
